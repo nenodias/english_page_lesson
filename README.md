7d4ecc86cefcb791071d6cc84e5317a348fb50f2
Horácio Dias Baptista Neto <idd_hbaptista@uolinc.com>
2021-04-11 19:31:46 -0300

    Alter to use aiohttp and doing async requests

--- README.md
+++ README.md
@@ -4,6 +4,3 @@
 python3 app.py lesson01.txt
 
 The script will generate a lesson01.html
-
-#### TODO
-Change requests for using async operation for improve speed
--- app.py
+++ app.py
@@ -1,56 +1,77 @@
 import pdb
 import json
 import sys
-import requests as r
+import aiohttp
+import asyncio
 
 sound_url = "https://api.soundoftext.com/sounds/"
 
+
 def get_word_payload(word):
     payload = {
-        "engine":"Google",
-        "data":{
-            "text":word,
-            "voice":"en-US"
+        "engine": "Google",
+        "data": {
+            "text": word,
+            "voice": "en-US"
         }
     }
     return payload
 
 
-def get_sound_word(word):
-    print("Getting the word %s" %(word))
+async def get_sound_word(r, word):
+    print("Getting the word %s" % (word))
     payload = get_word_payload(word)
-    headers = { "content-type":"application/json"}
-    response = r.post(sound_url, json.dumps(payload), headers=headers)
-    json_data = response.json()
+    headers = {"content-type": "application/json"}
+    response = await r.post(sound_url, json=payload)
+    json_data = await response.json()
     if json_data["success"]:
-        response = r.get(sound_url + json_data["id"], headers=headers)
-        json_data = response.json()
-        return { "filename": json_data["location"], "word":word }
+        response = await r.get(sound_url + json_data["id"])
+        json_data = await response.json()
+        return {"filename": json_data["location"], "word": word}
+    return {"filename": None, "word": word}
 
-template = ""
 
-with open("template.html", "r") as f:
-    template = f.read()
+async def get_words(r, words):
+    futures = []
+    for word in words:
+        futures.append(loop.create_task(get_sound_word(r, word)))
+    return await asyncio.gather(*futures)
 
-template_js = ""
 
-with open("template.js", "r") as f:
-    template_js = f.read()
+async def main():
+    async with aiohttp.ClientSession() as session:
+        template = ""
 
-words_template = ""
-
-if len(sys.argv) > 1:
-    filename = sys.argv[1]
-    output = filename.replace(".txt", ".html")
-
-    with open(filename, "r") as f:
-        words = f.read().split("\n")
-        words_template = ""
-        for index, word in enumerate(words):
-            data = get_sound_word(word)
-            words_template += template.format(index=index, word=word, filename=data["filename"])
-
-    with open("index.html", "r") as f:
-        with open(output, "w") as out:
+        with open("template.html", "r") as f:
             template = f.read()
-            out.write(template.format(script=template_js, words= words_template))
+
+        template_js = ""
+
+        with open("template.js", "r") as f:
+            template_js = f.read()
+
+        words_template = ""
+
+        if len(sys.argv) > 1:
+            filename = sys.argv[1]
+            output = filename.replace(".txt", ".html")
+
+            with open(filename, "r") as f:
+                words = f.read().split("\n")
+                words_template = ""
+                generator = await get_words(session, words)
+                for index, data in enumerate(generator):
+                    word = data["word"]
+                    words_template += template.format(
+                        index=index, word=word, filename=data["filename"])
+
+            with open("index.html", "r") as f:
+                with open(output, "w") as out:
+                    template = f.read()
+                    out.write(template.format(
+                        script=template_js, words=words_template))
+
+
+loop = asyncio.get_event_loop()
+tasks = [main()]
+loop.run_until_complete(asyncio.wait(tasks))
